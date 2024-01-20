9370c6f2a0a273db9d189480964fe0eb1000964d
technicallyawriter <npjones@protonmail.com>
2024-01-20 03:09:30 -0500

    remove subshell syntax

--- scripts/install_vim-plug.sh
+++ scripts/install_vim-plug.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-# Get current user and assign it to a variable
-current_user=$(whoami)
-
-# Echo the current user
-echo "Current User: $current_user"
-
 # install vim-plug via script
-echo "Installing vim-plug via script"
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# Change directory owner from root to $USER
-echo "Changing directory owner from root to current user"
-sudo chown -R $current_user:$current_user "${XDG_DATA_HOME:-$HOME}/.local/share/nvim"
+echo "Installing vim-plug via script..."
+if curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim; then
+  echo "vim-plug installed."
+else
+  echo "Failed to install vim-plug via script."
+fi
 
 # Run :PlugInstall
 echo "Running :PlugInstall..."
