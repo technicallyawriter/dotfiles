bc4da25e3beaa6e0d0ed1e047d34b8bc6b7ef398
technicallyawriter <npjones@protonmail.com>
2024-01-20 01:17:18 -0500

    add chown for vim-plug dirs

--- scripts/install_vim-plug.sh
+++ scripts/install_vim-plug.sh
@@ -2,8 +2,16 @@
 
 # install vim-plug via script
 echo "Installing vim-plug via script"
-sudo sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Change directory owner from root to $USER
+echo "Changing directory owner from root to current user"
+if sudo chown -R $USER:$USER $HOME/.local/share/nvim; then
+  echo "Success!"
+else
+  log_error "Failed to change directory owner"
+fi
 
 # Run :PlugInstall
 echo "Running :PlugInstall..."
