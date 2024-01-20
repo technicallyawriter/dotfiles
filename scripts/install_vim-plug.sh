50212fc40647826c2320733eb6d1e803046c4d3d
technicallyawriter <npjones@protonmail.com>
2024-01-20 00:26:38 -0500

    remove logic from nvim install script

--- scripts/install_vim-plug.sh
+++ scripts/install_vim-plug.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
+# install vim-plug via script
 echo "Installing vim-plug via script"
-if sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'; then
-  echo "vim-plug installed succcessfully."
-else
-  log_error "Failed to install vim-plug via script"
-fi
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Run :PlugInstall
 echo "Running :PlugInstall..."
